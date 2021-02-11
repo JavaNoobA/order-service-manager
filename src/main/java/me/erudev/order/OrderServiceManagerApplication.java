f265240f52bdb5ed1624defac4c3eb83e1d51ae8
zhaopengfei <1027926875@qq.com>
2021-02-11 13:35:36 +0800

    add order dao

--- src/main/java/me/erudev/order/OrderServiceManagerApplication.java
+++ src/main/java/me/erudev/order/OrderServiceManagerApplication.java
@@ -1,9 +1,11 @@
 package me.erudev.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"me.erudev.order.dao"})
 public class OrderServiceManagerApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/me/erudev/order/dao/OrderDetailDao.java
@@ -0,0 +1,28 @@
+package me.erudev.order.dao;
+
+import me.erudev.order.po.OrderDetailPO;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author pengfei.zhao
+ * @date 2021/2/11 13:19
+ */
+@Mapper
+@Repository
+public interface OrderDetailDao {
+    @Insert("INSERT INTO order_detail (status, address, account_id, product_id, deliveryman_id, settlement_id, " +
+            "reward_id, price, date) VALUES(#{status}, #{address},#{accountId},#{productId},#{deliverymanId}," +
+            "#{settlementId}, #{rewardId},#{price}, #{date})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void insert(OrderDetailPO orderDetailPO);
+
+    @Update("update order_detail set status =#{status}, address =#{address}, account_id =#{accountId}, " +
+            "product_id =#{productId}, deliveryman_id =#{deliverymanId}, settlement_id =#{settlementId}, " +
+            "reward_id =#{rewardId}, price =#{price}, date =#{date} where id=#{id}")
+    void update(OrderDetailPO orderDetailPO);
+
+    @Select("SELECT id,status,address,account_id accountId, product_id productId,deliveryman_id deliverymanId," +
+            "settlement_id settlementId,reward_id rewardId,price, date FROM order_detail WHERE id = #{id}")
+    OrderDetailPO selectOrder(Integer id);
+}
